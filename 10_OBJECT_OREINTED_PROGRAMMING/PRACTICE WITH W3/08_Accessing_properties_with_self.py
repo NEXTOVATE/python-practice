0d1128418fcf068d99291c3f03654cd648f09488
Vivek kumar <vk3052704@gmail.com>
2026-01-15 23:41:09 +0530

    15-01-2026

--- 10_OBJECT_OREINTED_PROGRAMMING/PRACTICE WITH W3/08_Accessing_properties_with_self.py
+++ 10_OBJECT_OREINTED_PROGRAMMING/PRACTICE WITH W3/08_Accessing_properties_with_self.py
@@ -1,1 +1,21 @@
 # You can access any property of the class using self:
+
+class Car:
+  def __init__(self, brand, model, year):
+    self.brand = brand
+    self.model = model
+    self.year = year
+
+  def display_info(self):
+    print(f"{self.year} {self.brand} {self.model}")
+
+car1 = Car("Toyota", "Corolla", 2020)
+car1.display_info()
+
+
+
+
+
+
+
+
