79a34c06cc9855bc11ab7e77761a0cd87137c36e
Vivek kumar <vk3052704@gmail.com>
2026-02-07 22:55:15 +0530

    s

---
+++ 10_OBJECT_OREINTED_PROGRAMMING/PRACTICE WITH W3/10_modify_properties.py
@@ -0,0 +1,14 @@
+# You can modify the value of properties on objects:
+# Change the age property:
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+
+    
+P1 = Person("Monu",13)
+
+P1.age = 15             #modify the age 
+print(P1.name,P1.age)
