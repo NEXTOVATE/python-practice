2e7aa2f49ac574e3360fc58cd7c7e636c24a096f
Vivek kumar <vk3052704@gmail.com>
2026-02-11 23:19:31 +0530

    d

--- 10_OBJECT_OREINTED_PROGRAMMING/05_default_parameter.py
+++ 10_OBJECT_OREINTED_PROGRAMMING/05_default_parameter.py
@@ -1,2 +1,12 @@
+class Boy():
+    def __init__(self,name,age=17):
+        self.name = name
+        self.age = age
 
+
+B1 = Boy("Prince")      #In B1 object the default age of Prince is 17
+B2 = Boy("Mayank",16)
+
+print(B1.name,B1.age)
+print(B2.name,B2.age)
 
