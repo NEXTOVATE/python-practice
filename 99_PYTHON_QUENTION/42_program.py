e138b0ae47072983ce6e00888c1c039f99c5cee5
Vivek kumar <vk3052704@gmail.com>
2026-02-22 22:53:36 +0530

    22-02-2026

---
+++ 99_PYTHON_QUENTION/42_program.py
@@ -0,0 +1,26 @@
+# Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+# The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+# Mind the input validation.
+
+# Example
+# { 6, 2, 1, 8, 10 } => 16
+# { 1, 1, 11, 2, 3 } => 6
+# Input validation
+# If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+
+def sum_arry(l):
+    if len(l) <3 and len(l) == 0:
+        return 0
+    else:
+        l.remove(min(l))
+        l.remove(max(l))
+        return sum(l)
+
+
+
+l = [1, 1,1] 
+# l = [6, 2, 1, 8, 10]
+print(sum_arry(l))
--- 99_PYTHON_QUENTION/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-s = "monik"
-
-l = int((len(s)/2))
-print(s[l])
